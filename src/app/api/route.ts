7d14852a0624cec12531a66682f084ad4de1aa49
shahjalal <lx.shahjalal@gmail.com>
2024-06-25 13:14:50 +0600

    build fix

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export const GET = (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("req", req);
-  console.log("res", res);
-  return res
-};
