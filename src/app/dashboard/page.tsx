e02341498a4884615388b639721bbc8972d30687
shahjalal <lx.shahjalal@gmail.com>
2024-07-01 09:18:29 +0600

    build fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,3 @@
-import { getProducts } from '@/server/product-action';
-
 export default async function page() {
-  const data = await getProducts();
   return <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">Dashboard</div>;
 }
