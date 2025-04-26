0267efca75e5e01c880ac0bc0f0417b917b73152
Shahjalal <38864447+m-shahjalal@users.noreply.github.com>
2025-04-26 22:51:18 +0600

    change redirect url

--- src/utils/auth.action.ts
+++ src/utils/auth.action.ts
@@ -1,20 +1,20 @@
-'use server';
+"use server";
 
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import { createClient } from '@/utils/supabase-server';
-import { LoginValuesType } from '@/validator/login-form-schema';
-import { User } from '@supabase/supabase-js';
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase-server";
+import { LoginValuesType } from "@/validator/login-form-schema";
+import { User } from "@supabase/supabase-js";
 
-import { PAGES } from '@/config/pages';
+import { PAGES } from "@/config/pages";
 
 const getURL = () => {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ??
     process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-    'http://localhost:3000/';
-  url = url.startsWith('http') ? url : `https://${url}`;
-  url = url.endsWith('/') ? url : `${url}/`;
+    "https://shocchocroy.com/";
+  url = url.startsWith("http") ? url : `https://${url}`;
+  url = url.endsWith("/") ? url : `${url}/`;
   return url;
 };
 
@@ -29,7 +29,7 @@ export async function login(data: LoginValuesType) {
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) redirect(PAGES.ERROR);
-  revalidatePath(PAGES.ROOT, 'layout');
+  revalidatePath(PAGES.ROOT, "layout");
   redirect(PAGES.ROOT);
 }
 
@@ -38,21 +38,21 @@ export async function signup(data: LoginValuesType) {
   const { error } = await supabase.auth.signUp(data);
 
   if (error) redirect(PAGES.ERROR);
-  revalidatePath(PAGES.ROOT, 'layout');
+  revalidatePath(PAGES.ROOT, "layout");
   redirect(PAGES.ROOT);
 }
 
 export const googleLogin = async () => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
-    provider: 'google',
+    provider: "google",
     options: { redirectTo: getURL() },
   });
 
   if (data.url) redirect(data.url);
   if (error) redirect(PAGES.ERROR);
-  
-  revalidatePath(PAGES.ROOT, 'layout');
+
+  revalidatePath(PAGES.ROOT, "layout");
   redirect(PAGES.ROOT);
 };
 
@@ -61,7 +61,7 @@ export const logout = async () => {
   const { error } = await supabase.auth.signOut();
 
   if (error) redirect(PAGES.ERROR);
-  revalidatePath(PAGES.ROOT, 'layout');
+  revalidatePath(PAGES.ROOT, "layout");
   redirect(PAGES.LOGIN);
 };
 
